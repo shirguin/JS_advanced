c94571d7201eba8a346029b0cfbcfc44949286c4
shirguin <shirguin@yandex.ru>
2023-10-11 06:34:43 +0300

    hw_seminar_3_3

--- hw_seminar_3/script_1.js
+++ hw_seminar_3/script_1.js
@@ -1,38 +1,60 @@
-
-const contentEl = document.querySelector(".content");
+const productsListEl = document.querySelector(".products");
 
 if (localStorage.length > 0) {
-    let products = getProducts();
+  let products = getProducts();
 
-    console.log(products);
-    /* let keys = Object.keys(products);
-    console.log(keys);
-
-    console.log(products.key(0)); */
-
-
-    /*     for (index in products) {
-            console.log(products[index]);
-            console.log(products.getItem(products.key(index)));
-        } */
-    /*     for (key in products) {
-            if (products.hasOwnProperty(key)) {
-                //ключ = key
-                //значение = person[key]
-                console.log("Ключ = " + key);
-                console.log("Значение = " + products[key]);
-            } // если объект person имеет key (если у person есть свойство key)
-        } // перерабрать все ключи (свойства) в объекте */
-
+  products.forEach((product) => {
+    let newLiEl = document.createElement("li");
+    newLiEl.className = "productName";
+    newLiEl.innerHTML = `<h4>${product.name}</h4>`;
+    productsListEl.append(newLiEl);
+  });
 }
 
+//Навешиваем обработчик на товары (Показ отзывов)
+document.querySelectorAll(".productName").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    //Написать проверку, что уже открыты отзывы!!!
+    let productName = e.target.innerHTML;
+    let comments = getComments(productName);
 
+    let newUlEl = document.createElement("ul");
+    newUlEl.className = "productComments";
+    e.target.parentNode.append(newUlEl);
+
+    comments.forEach((comment) => {
+      let newLiEl = document.createElement("li");
+      newLiEl.className = "comment";
+      newLiEl.innerHTML = `
+        <div class="commentItem">
+          <p>${comment}</p>
+          <button class="delete button">Удалить отзыв</button>
+        </div>
+          `;
+      newUlEl.append(newLiEl);
+    });
+  });
+});
+
+//Получаем массив объектов товаров, хранящихся в localStorage
 function getProducts() {
-    let products = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        console.log(key);
-        products[key] = JSON.parse(localStorage.getItem(key));
+  let products = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    let productName = localStorage.key(i);
+    let productComments = JSON.parse(localStorage.getItem(productName));
+    let product = { name: productName, comments: productComments };
+    products.push(product);
+  }
+  return products;
+}
+
+//Получаем все отзывы о товаре по имени товара
+function getComments(productName) {
+  let products = getProducts();
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].name === productName) {
+      return products[i].comments;
     }
-    return products;
+  }
 }
